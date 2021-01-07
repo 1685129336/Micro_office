15775546e12fbaab802644e76818647fd6ae5be8
1685129336 <1685129336@qq.com>
2021-01-07 19:52:34 +0800

    用户中心模块

---
+++ usercenter/src/main/java/com/bawei/usercenter/UserCenterActivity.kt
@@ -0,0 +1,20 @@
+package com.bawei.usercenter
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import core.ui.BaseActivity
+
+class UserCenterActivity : BaseActivity(){
+    override fun initData() {
+
+    }
+
+    override fun initView() {
+
+    }
+
+    override fun layoutID(): Int {
+        return R.layout.activity_user_center
+    }
+
+}
