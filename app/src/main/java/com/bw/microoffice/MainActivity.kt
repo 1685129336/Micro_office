5ab1f96155fcc33d707124141e03f3bdd794b921
blackCat8888 <1207246760@qq.com>
2021-01-09 11:09:26 +0800

    修改

--- app/src/main/java/com/bw/microoffice/MainActivity.kt
+++ app/src/main/java/com/bw/microoffice/MainActivity.kt
@@ -13,26 +13,26 @@ import java.util.*
 class MainActivity : AppCompatActivity() {
     private var maincusList = ArrayList<CustomTabEntity>()
     private var mainfragmentList = ArrayList<Fragment>()
-    lateinit var  homeFragment:HomeFragment
+//    lateinit var  homeFragment:HomeFragment
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        homeFragment = HomeFragment()
+//        homeFragment = HomeFragment()
 
 
-        mainfragmentList.add(homeFragment)
+//        mainfragmentList.add(homeFragment)
 //        mainfragmentList.add(homeFragment)
 //        mainfragmentList.add(homeFragment)
 //        mainfragmentList.add(homeFragment)
 //        mainfragmentList.add(homeFragment)
 
-        maincusList.add(MainCuBean("消息", 0, 0))
+//        maincusList.add(MainCuBean("消息", 0, 0))
 //        maincusList.add(MainCuBean("日历", 0, 0))
 //        maincusList.add(MainCuBean("工作台", 0, 0))
 //        maincusList.add(MainCuBean("云空间", 0, 0))
 //        maincusList.add(MainCuBean("联系人", 0, 0))
 
-        main_commtab.setTabData(maincusList, this, R.id.main_fram, mainfragmentList)
+//        main_commtab.setTabData(maincusList, this, R.id.main_fram, mainfragmentList)
 
     }
 
