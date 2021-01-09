3e07a6af32f93144c5642d98615510379a15fdb5
blackCat8888 <1207246760@qq.com>
2021-01-09 11:13:13 +0800

    修改

--- app/src/main/java/com/bw/microoffice/MainActivity.kt
+++ app/src/main/java/com/bw/microoffice/MainActivity.kt
@@ -3,7 +3,7 @@ package com.bw.microoffice
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
-import com.example.home.fragment.HomeFragment
+//import com.example.home.fragment.HomeFragment
 
 //import com.example.home.fragment.HomeFragment
 import com.flyco.tablayout.listener.CustomTabEntity
